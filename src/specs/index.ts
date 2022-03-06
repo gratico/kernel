6767ce56ae2453ff637216e680adfa8b5848f2d1
Abhishiv Saxena <abhishiv@gmail.com>
2022-03-06 06:08:32 +0200

    split out @gratico/runtime

--- src/specs/index.ts
+++ src/specs/index.ts
@@ -1,7 +1,4 @@
 export type { IFileSystem } from '@gratico/fs'
 export * from './kernel'
-export * from './runtime'
-export * from './pm'
-export * from './npm'
 export * from './business'
 export * from './emitter'
